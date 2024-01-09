ddef1ec83ab5a843e0f0f800f9c3ba65d531b5b9
Pedro Henirique <heroperde@gmail.com>
2024-01-09 18:39:44 -0300

    improving the implementation of the printChessBoard method

--- Chess-Console/Program.cs
+++ Chess-Console/Program.cs
@@ -10,9 +10,19 @@ namespace Chess
     {
         static void Main(string[] args) 
         {
-            PositionChess pos = new PositionChess('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosition());
+            try
+            {
+                ChessBoard board = new ChessBoard(8, 8);
+                board.putPart(new Castle(board, Color.Black), new Position(1, 2));
+                board.putPart(new Castle(board, Color.Black), new Position(0, 3));
+                board.putPart(new King(board, Color.Black), new Position(1, 3));
+                board.putPart(new Castle(board, Color.White), new Position(3, 5));
+                Screen.PrintChessBoard(board);
+            }
+            catch (ChessExcepetion e) 
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- Chess-Console/Screen.cs
+++ Chess-Console/Screen.cs
@@ -1,6 +1,7 @@
 ﻿using Board;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,13 @@ namespace Chess
         {
             for (int i = 0; i < board.Lines; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.Piece(i, j) != null)
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        PrintPart(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                     else
                     {
@@ -25,6 +28,22 @@ namespace Chess
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void PrintPart(Part part) 
+        {
+            if(part.ColorPart == Color.White) 
+            {
+                Console.Write(part);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(part);
+                Console.ForegroundColor = aux;
             }
         }
     }
